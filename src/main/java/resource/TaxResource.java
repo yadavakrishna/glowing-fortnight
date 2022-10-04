e84ba54fdc755c4ff3511ba054736c29e6273ec9
yadavakrishna <pranayyadav950@gmail.com>
2022-10-04 23:40:39 +0530

    Tax Calculation

--- src/main/java/resource/TaxResource.java
+++ src/main/java/resource/TaxResource.java
@@ -8,8 +8,25 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class TaxResource {
 
-    //TODO: 1st Input Tax Slab, 2nd total - JSON containing all tax information
-    public void calculateTax(String taxSlab) {
+    public double calculateTax(double income) {
+        double ans = 0;
 
+        if (income < 250000) {
+            ans += 0;
+        } else if (income < 500000) {
+            ans += 0.05 * (income - 250000);
+        } else if (income < 750000) {
+            ans += (0.05 * 250000) + (0.10 * (income - 500000));
+        } else if (income < 1000000) {
+            ans += (0.05 * 250000) + (0.10 * 250000) + (0.15 * (income - 750000));
+        } else if (income < 1250000) {
+            ans += (0.05 * 250000) + (0.10 * 250000) + (0.15 * 250000) + (0.20 * (income - 1000000));
+        } else if (income < 1500000) {
+            ans += (0.05 * 250000) + (0.10 * 250000) + (0.15 * 250000) + (0.20 * 250000) + (0.25 * (income - 1250000));
+        } else if (income >= 1500000) {
+            ans += (0.05 * 250000) + (0.10 * 250000) + (0.15 * 250000) + (0.20 * 250000) + (0.25 * 250000) + (0.30 * (income - 1500000));
+        }
+
+        return ans;
     }
 }
